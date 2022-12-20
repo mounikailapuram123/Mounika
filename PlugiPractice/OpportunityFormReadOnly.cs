ab9ac2e2fe87530b8a86a524c65c9a39d3ff596a
Ravi <Ravi@DESKTOP-TN4PAEB>
2022-12-20 11:38:43 +0530

    Add project files.

---
+++ PlugiPractice/OpportunityFormReadOnly.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlugiPractice
+{
+    public class OpportunityFormReadOnly : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+            IOrganizationServiceFactory organizationService = serviceProvider.GetService(typeof(IOrganizationServiceFactory)) as IOrganizationServiceFactory;
+            IOrganizationService service = organizationService.CreateOrganizationService(context.UserId);
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                try
+                {
+
+                    int accessLevel = ((OptionSetValue)entity["sknd_acceslevel"]).Value;
+                    string managemnt = "08BDE210-877F-ED11-81AD-000D3A5656E9";
+                    string operationnal = "D8283DB1-867F-ED11-81AD-000D3A5656E9";
+                    string financial = "662E5CE1-867F-ED11-81AD-000D3A5656E9";
+
+                    if (accessLevel == 1)
+                    {
+                        GetUserOfTeams(service, financial, entity.Id);
+                    }
+                    else if (accessLevel == 2)
+                    {
+                        GetUserOfTeams(service, managemnt, entity.Id);
+                    }
+                    else if (accessLevel == 3)
+                    {
+                        GetUserOfTeams(service, operationnal, entity.Id);
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            // throw new NotImplementedException();
+        }
+
+
+
+        private static void GetUserOfTeams(IOrganizationService service, string team, Guid recordaID)
+        {
+            string fetch = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='true'>" +
+                                            "<entity name='systemuser'>" +
+                                              "<attribute name='fullname' />" +
+                                              "<attribute name='businessunitid' />" +
+                                              "<attribute name='title' />" +
+                                              "<attribute name='address1_telephone1' />" +
+                                              "<attribute name='positionid' />" +
+                                              "<attribute name='systemuserid' />" +
+                                              "<order attribute='fullname' descending='false' />" +
+                                              "<filter type='and'>" +
+                                                "<condition attribute='domainname' operator='not-null' />" +
+                                              "</filter>" +
+                                              "<link-entity name='teammembership' from='systemuserid' to='systemuserid' visible='false' intersect='true'>" +
+                                                "<link-entity name='team' from='teamid' to='teamid' alias='ag'>" +
+                                                  "<filter type='and'>" +
+                                                    "<condition attribute='teamid' operator='eq' uiname='' uitype='team' value='" + team + "' />" +
+                                                  "</filter>" +
+                                                "</link-entity>" +
+                                              "</link-entity>" +
+                                            "</entity>" +
+                                          "</fetch>";
+            EntityCollection entityCollection = service.RetrieveMultiple(new FetchExpression(fetch));
+            if (entityCollection != null && entityCollection.Entities != null && entityCollection.Entities.Count > 0)
+            {
+
+                makeFormReadOnly(service, recordaID);
+            }
+
+
+        }
+        private static void makeFormReadOnly(IOrganizationService service, Guid recordId)
+        {
+            Entity opportunity = new Entity("opportunity");
+            opportunity["statuscode"] = new OptionSetValue(2);
+            opportunity.Id = recordId;
+            service.Update(opportunity);
+
+
+            //var stateRef = new EntityReference("opportunity", recordId);
+            //SetStateRequest req = new SetStateRequest();
+            //req.State = new OptionSetValue(0);
+            //req.Status = new OptionSetValue(2);
+            //req.EntityMoniker = stateRef;
+            //SetStateResponse stateSet = (SetStateResponse)service.Execute(req);
+        }
+
+    }
+}
---
+++ PlugiPractice/PostOpearionCreateAccount.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class PostOpearionCreateAccount : IPlugin
+    {
+        private readonly string _unsecureString;
+        private readonly string _secureString;
+
+        public PostOpearionCreateAccount(string unsecureString, string secureString)
+        {
+            if (String.IsNullOrWhiteSpace(unsecureString) &&
+                String.IsNullOrWhiteSpace(secureString))
+            {
+                throw new InvalidPluginExecutionException("Unsecure and secure strings are required for this plugin to execute.");
+            }
+
+            _unsecureString = unsecureString;
+            _secureString = secureString;
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+            IOrganizationServiceFactory organizationService = serviceProvider.GetService(typeof(IOrganizationServiceFactory)) as IOrganizationServiceFactory;
+            IOrganizationService service = organizationService.CreateOrganizationService(context.UserId);
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity entity = (Entity)context.InputParameters["Target"];
+                try
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        var account = new AccountDetails();
+                        account.FirstName = entity.Attributes["name"].ToString();
+                        account.LastName = entity.Attributes["accountnumber"].ToString();
+                        DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(AccountDetails));
+                        MemoryStream memoryStream = new MemoryStream();
+                        serializer.WriteObject(memoryStream, account);
+                        var jsonObject = Encoding.Default.GetString(memoryStream.ToArray());
+                        var webClient = new WebClient();
+                        webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                        var serviceUrl = _secureString;
+                        // upload the data using Post mehtod
+                        string response = webClient.UploadString(serviceUrl, jsonObject);
+                        Entity acount = new Entity("account");
+                        acount.Id = entity.Id;
+                        acount["address1_line1"] = response;
+                        acount["telephone1"] = _unsecureString;
+
+                        service.Update(acount);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidPluginExecutionException(ex.Message);
+                }
+            }
+        }
+        public class AccountDetails
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
